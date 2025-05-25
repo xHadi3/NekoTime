3915df8059ea8d7147bc0dae4827eaf94801f86d
xHadi3 <hadiessam9@gmail.com>
2025-05-25 13:13:57 +0300

    Initial Commit

---
+++ NekoTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NekoTime
+//
+//  Created by Hadi Al zayer on 27/11/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NekoTime/NekoTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NekoTimeApp.swift
+//  NekoTime
+//
+//  Created by Hadi Al zayer on 27/11/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct NekoTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
